793cbbefa026d1a47a6a38f0a84b2316854d4b5f
hackzoomuck <xaq03061@gmail.com>
2020-08-10 10:25:46 +0900

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post, Comment
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','count_text']
+    list_display_links = ['title']
+
+    def count_text(self, obj):
+        return '{}글자'.format(len(obj.text))
+    count_text.short_description = 'text 글자수'
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
